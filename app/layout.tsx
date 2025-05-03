f0815c090701771144b1141bca7c2e6d72ccce8d
ratikkoka <koka.ratik@yahoo.com>
2025-05-02 21:23:20 -0700

    chore: 🔧 add vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css';
 import { Metadata, Viewport } from 'next';
 import clsx from 'clsx';
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import { Providers } from './providers';
 
@@ -52,6 +53,7 @@ export default function RootLayout({
           <MainLayout>{children}</MainLayout>
         </Providers>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
